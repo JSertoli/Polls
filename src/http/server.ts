851b47a4c5d117408e5d64503b1483ded6d693c9
Joao Sertoli <jgsertoli@gmail.com>
2024-02-08 22:42:22 -0300

    Create method for vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
+import { fastifyWebsocket } from '@fastify/websocket';
+import { pollResults } from './ws/poll-results';
 
 const app = fastify();
 
@@ -11,9 +13,13 @@ app.register(cookie, {
     secret: 'api-polls-secret',
     hook: 'onRequest',
 })
+
+app.register(fastifyWebsocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
